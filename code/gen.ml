e903458b1e8efbbdbe2648bfe491a9eeae636105
szdane <sdane@umich.edu>
2025-06-24 14:37:34 -0400

    env vars translation added

--- code/gen.ml
+++ code/gen.ml
@@ -26,31 +26,47 @@
       if old_var = new_var then pred
       else
         Str.global_replace (Str.regexp_string old_var) new_var pred
+    
+    let bind_printer i env =  "bind " ^ i ^ " " ^ env   
+    
+    let rec all_bindings n i env = 
+      match env with 
+        | [] -> "" 
+        | x::xs -> (bind_printer (string_of_int i) x) ^ "\n" ^ (all_bindings n (i+1) xs)
+    
+    let env_elem n = string_of_int n ^ ";"
+
+    let rec env_list n = match n with
+      |0->""
+      |1->string_of_int n
+      |_ -> env_elem n ^ " " ^ env_list (n-1)
   
-  
-    let refined_pair_to_fq ?(cid = 1) ?(bid = 1) t1 t2 =
+    let refined_pair_to_fq ?(cid = 1) ?(bid = 1) t1 t2 env =
       let v1, sort1, p1 = parse_refined t1 in
       let v2, sort2, p2_raw = parse_refined t2 in
       if sort1 <> sort2 then
         failwith ("Sort mismatch: " ^ sort1 ^ " vs " ^ sort2);
-  
-      let p2 = alpha_convert ~old_var:v2 ~new_var:v1 p2_raw in
-  
+      
+      let p2 = alpha_convert ~old_var:v2 ~new_var:v1 p2_raw 
+      and binds = all_bindings (List.length env) 1 env
+      and env_vars = env_list (List.length env) in
       Printf.sprintf
-        "constraint:\n\
-         \  env []\n\
+        "%s \n\
+        constraint:\n\
+         \  env [%s]\n\
          \  lhs {%s:%s | %s}\n\
          \  rhs {%s:%s | %s}\n\
          \  id  %d tag []"
-        v1 sort1 p1 v1 sort1 p2 cid
+      binds env_vars v1 sort1 p1 v1 sort1 p2 cid
   end
   
   let () =
-  let t_left  = "{x:Int | x<0}"
-  and t_right = "{y:Int | y<5}" in
+  let t_left  = "{x:Int | x<u}"
+  and t_right = "{y:Int | y<k}" 
+  and env = "u: {v: int | v = 5}"::"k: {v: Int | v=7}"::[] in
 
   let fq_text =
-    Refined_to_fq.refined_pair_to_fq ~cid:1 ~bid:1 t_left t_right in
+    Refined_to_fq.refined_pair_to_fq ~cid:1 ~bid:1 t_left t_right env in
 
   (* Output filename: first CLI arg or default "subtyping.fq" *)
   let out_file =
